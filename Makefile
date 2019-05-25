bd93c05f92873e7e16f22eaab4c3be973fd2a365
dlwlgus53 <dlwlgus4553@gmail.com>
2019-05-25 11:34:27 +0900

    unlock hook

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
 all:
-	gcc -o test1 test1.c
 	gcc -shared -fPIC -o myrand.so myrand.c -ldl
 	gcc -shared -fPIC -o myfinder.so myfinder.c -ldl
--- myfinder.c
+++ myfinder.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <dlfcn.h>
 #include <execinfo.h>
+
 int 
 pthread_mutex_lock (pthread_mutex_t *mutex)
 {
@@ -13,9 +14,27 @@ pthread_mutex_lock (pthread_mutex_t *mutex)
 	lockp = dlsym(RTLD_NEXT, "pthread_mutex_lock") ;
 	if ((error = dlerror()) != 0x0) 
 		exit(1) ;
+    	lockp(mutex);
 
 	char buf[50] ;
 	snprintf(buf, 50, "in\n") ;
+	fputs(buf, stderr) ;
+	return 77 ; 	
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+	int  (*ulockp)(pthread_mutex_t *mutex) ; 
+	char * error ;
+	
+	ulockp = dlsym(RTLD_NEXT, "pthread_mutex_unlock") ;
+	if ((error = dlerror()) != 0x0) 
+		exit(1) ;
+    
+    	ulockp(mutex);
+    
+	char buf[50] ;
+	snprintf(buf, 50, "out\n") ;
 	fputs(buf, stderr) ;
 	return 77 ; 	
 }
