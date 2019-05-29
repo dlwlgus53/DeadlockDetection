1915f07ad7e112e09b9aeb6326ef1c599a9327cc
hyeongjunKim <xyz2919@gmail.com>
2019-05-29 16:17:14 +0900

    dpredicter.c edited

--- dpredicter.c
+++ dpredicter.c
@@ -31,22 +31,31 @@ int main (int argc,char **argv){
     char data[maxDataLen];
     fp=fopen(argv[1],"r");
     int length = 0;
+    int line_count = 1;
     if (argc != 2){
         printf("interface error!\n");
     }
-    while(fscanf(fp,"%s",buffer)>0)
+
+    /*read only line 1 && line 3 in dmonitor.trace*/
+    while(fgets(buffer,maxBufLen,fp) !=0x00)
     {
-        if((length+strlen(buffer)+1)>=maxDataLen)break;
+        if(line_count==1 ||line_count==3 ){
+        if((length+strlen(buffer))>=maxDataLen)break;
         strncpy(&data[length],buffer,strlen(buffer));
-        length += strlen(buffer)+1;
+        length += strlen(buffer);
         data[length-1]=' ';
+        //data[length-1]='\0';
+        //break;
+        }
+        line_count ++;
+        
     }
-    data[length] = 0x00;
+    data[length]='\0';
     //printf("The string is : \n%s\n",data);
     fclose(fp);
 
      /* Make Tokens */
-    token_t tokens[100];
+    token_t tokens[10];
     /*Declaration Variables*/
     token_t* token;
     parser p;
@@ -78,25 +87,55 @@ int main (int argc,char **argv){
                     }
             }
             break;
+
+        case '/': 
+            token = alloc_token(&p,tokens,100);
+        
+            if (token == NULL) {
+                    return -1;
+            }
+            token->start = p.pos;
+            break;
+
+        case '(':
+            for (i = p.toknext - 1; i >= 0; i--) {
+                    token = &tokens[i];
+                    if (token->start != -1 && token->end == -1) {
+                        token->end = p.pos + 1;
+                        break;
+                    }
+            }
+            break;
+
         default:
             break;
         }
     }
 
-    /*confirm address*/
+    /*confirm address & exec file name*/
+    //printf("%.*s\n",tokens[0].end-tokens[0].start-2,data+tokens[0].start+1);
     //printf("%.*s\n",tokens[1].end-tokens[1].start-2,data+tokens[1].start+1);
     /*copy address*/
-    char address[50];
+    char address[30];
     strncpy(address,data+tokens[1].start+1,tokens[1].end-tokens[1].start-2);
-   // printf("%s\n",address);
+    //printf("The address is : \n%s\n",address);
+
+    char filename[30];
+    strncpy(filename,data+tokens[0].start+1,tokens[0].end-tokens[0].start-2);
+    //printf("The filename is : \n%s\n",filename);
 
 
     /*addr2line*/
-    char command[50];
-    strcpy(command,"addr2line -e dinning ");
-    strcat(command,address);
+     char command[50];
+     strcpy(command,"addr2line -e ");
+     strcat(command,filename);
+     strcat(command," ");
+     strcat(command, address);
+    
+        
     system(command);
-
+    //printf("The command is : \n%s\n",command);
+    printf("The number of threads which may trigger DEADLOCK is %c\n",data[0]);
     return(0);
 
 
